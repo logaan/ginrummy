4d65cdb6ec64cfec8faf8be099b06ac4a7036ed8
Colin Campbell-McPherson <colin@logaan.net>
2010-03-02 21:07:38 +0800

    game is started using game server only if no game is running already

--- src/game_controller.erl
+++ src/game_controller.erl
@@ -3,23 +3,28 @@
 -include("records.hrl").
 
 handle_request("start",[]) ->
-    PlayerOneName = beepbeep_args:get_param("player_one_name",Env),
-    PlayerTwoName = beepbeep_args:get_param("player_two_name",Env),
+  case lists:member(game_server, registered()) of
+    false ->
+      PlayerOneName = beepbeep_args:get_param("player_one_name",Env),
+      PlayerTwoName = beepbeep_args:get_param("player_two_name",Env),
+      game_server:start(PlayerOneName, PlayerTwoName);
+    true -> ok
+  end,
 
-    Game             = gin_rummy:start_game(PlayerOneName, PlayerTwoName),
-    PlayerOne        = Game#game.player1,
-    PlayerTwo        = Game#game.player2,
+  {game_state, Game} = gen_server:call(game_server, game_state),
+  PlayerOne          = Game#game.player1,
+  PlayerTwo          = Game#game.player2,
 
-    {render,"game/start.html",[
-      {game,            io_lib:print(Game)},
-      {player_one_name, PlayerOne#player.name},
-      {player_two_name, PlayerTwo#player.name},
-      {card_count,      length(PlayerOne#player.hand)},
-      {your_hand,       card_list(PlayerOne)},
-      {top_of_discard,  top_of_discard(Game#game.discard)},
-      {deck_size,       length(Game#game.deck)},
-      {opponent_size,   length(PlayerTwo#player.hand)}
-    ]}.
+  {render,"game/start.html",[
+    {game,            io_lib:print(Game)},
+    {player_one_name, PlayerOne#player.name},
+    {player_two_name, PlayerTwo#player.name},
+    {card_count,      length(PlayerOne#player.hand)},
+    {your_hand,       card_list(PlayerOne)},
+    {top_of_discard,  top_of_discard(Game#game.discard)},
+    {deck_size,       length(Game#game.deck)},
+    {opponent_size,   length(PlayerTwo#player.hand)}
+  ]}.
 
 card_list(Player) ->
   Hand = Player#player.hand,
--- src/game_server.erl
+++ src/game_server.erl
@@ -21,7 +21,8 @@ handle_call(game_state, _From, State) ->
     {reply, {game_state, State}, State}.
 
 start(Player1Name, Player2Name) ->
-  GameName = list_to_atom(string:join([io_lib:print(?MODULE), Player1Name, Player2Name], "_")),
+  % GameName = list_to_atom(string:join([io_lib:print(?MODULE), Player1Name, Player2Name], "_")),
+  GameName = game_server,
   gen_server:start_link({local, GameName}, ?MODULE, [Player1Name, Player2Name], []).
 
 stop() ->
