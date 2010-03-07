ac8ad1584ed371a4f1d629b4f0ceab04de9fce27
Colin Campbell-McPherson <colin@logaan.net>
2010-03-07 13:13:38 +0800

    chat server chan handle subscriptions, listeners, and broadcasts

--- src/chat_server.erl
+++ src/chat_server.erl
@@ -10,7 +10,7 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0, subscribe/2, state/1, broadcast/2]).
+-export([start_link/0, subscribe/1, listen/2, state/0, broadcast/1, test/0, test2/0]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -18,6 +18,7 @@
 
 -record(state, {subscribers=[], listeners=[]}).
 -record(subscriber, {name, messages=[]}).
+-record(listener, {name, process_id}).
 
 %%====================================================================
 %% API
@@ -28,12 +29,14 @@
 %%--------------------------------------------------------------------
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
-subscribe(Name, Pid) ->
-  gen_server:cast(Pid, {subscribe, Name}).
-state(Pid) ->
-  gen_server:call(Pid, state).
-broadcast(Message, Pid) ->
-  gen_server:cast(Pid, {broadcast, Message}).
+subscribe(Name) ->
+  gen_server:cast(?SERVER, {subscribe, Name}).
+listen(Name, Pid) ->
+  gen_server:cast(?SERVER, {listen, Name, Pid}).
+state() ->
+  gen_server:call(?SERVER, state).
+broadcast(Message) ->
+  gen_server:cast(?SERVER, {broadcast, Message}).
 
 %%====================================================================
 %% gen_server callbacks
@@ -75,8 +78,13 @@ handle_cast({broadcast, Message}, State = #state{ subscribers=Subscribers }) ->
   AddMessage = fun(Subscriber = #subscriber{ messages=Messages }) ->
     Subscriber#subscriber{ messages=[Message|Messages] }
   end,
-  NewSubscribers = lists:map(AddMessage, Subscribers),
-  {noreply, State#state{ subscribers=NewSubscribers } };
+  NewState = State#state{ subscribers=lists:map(AddMessage, Subscribers) },
+  {noreply, update_listeners(NewState)};
+
+handle_cast({listen, Name, ProcessID}, State = #state{ listeners=Listeners }) ->
+  NewListener = #listener{ name=Name, process_id=ProcessID },
+  NewState = State#state{ listeners=[NewListener|Listeners] },
+  {noreply, update_listeners(NewState) };
 
 handle_cast({subscribe, Name}, State = #state{ subscribers=Subscribers }) ->
   NewSubscriber = #subscriber{ name=Name },
@@ -114,3 +122,47 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 %%% Internal functions
 %%--------------------------------------------------------------------
+update_listeners(State = #state{ subscribers=Subscribers, listeners=Listeners }) ->
+  spawn(fun() -> [
+    Listener#listener.process_id !
+    {chat_messages, Subscriber#subscriber.name, Subscriber#subscriber.messages}    ||
+    Subscriber <- Subscribers,
+    Listener <- Listeners,
+    Subscriber#subscriber.name == Listener#listener.name,
+    Subscriber#subscriber.messages =/= []
+  ] end),
+  ClearDeliveredMail = fun(Subscriber = #subscriber{ name=Name }) ->
+    case lists:keymember(Name, 2, Listeners) of
+      true  -> Subscriber#subscriber{ messages=[] };
+      false -> Subscriber
+    end
+  end,
+  NewSubscribers = lists:map(ClearDeliveredMail, Subscribers),
+  State#state{ subscribers=NewSubscribers }.
+
+%%--------------------------------------------------------------------
+%%% Unit tests
+%%--------------------------------------------------------------------
+test() ->
+  Printer = spawn(fun print_messages/0),
+  chat_server:start_link(),
+  chat_server:subscribe(logan),
+  chat_server:broadcast("hey"),
+  chat_server:subscribe(royce),
+  chat_server:broadcast("lol"),
+  chat_server:subscribe(lyndon),
+  chat_server:broadcast("meow"),
+  chat_server:listen(logan, Printer),
+  chat_server:listen(lyndon, Printer),
+  timer:sleep(100),
+  chat_server:state().
+
+test2() ->
+  Printer = spawn(fun print_messages/0),
+  chat_server:listen(royce, Printer).
+
+print_messages() ->
+  receive
+    Message -> io:format("~p~n", [Message])
+  end,
+  print_messages().
