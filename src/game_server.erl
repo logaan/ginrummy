1e09a3397ee70f4687e5833d925d139b9d63c772
Colin Campbell-McPherson <colin@logaan.net>
2010-03-10 02:29:18 +0800

    switched uuid to a simple id server

--- src/game_server.erl
+++ src/game_server.erl
@@ -35,7 +35,8 @@ handle_call(game_state, _From, State) ->
   {reply, {game_state, State}, State}.
 
 start(Player1Name, Player2Name) ->
-  GameName = list_to_atom(uuid:to_string(uuid:v4())),
+  {new_id, Id} = id_server:new_id(),
+  GameName = list_to_atom(lists:concat(["g", Id])),
   {GameName, gen_server:start_link({local, GameName}, ?MODULE, [Player1Name, Player2Name], [])}.
 
 stop() ->
--- src/ginrummy_app.erl
+++ src/ginrummy_app.erl
@@ -7,10 +7,12 @@
 %% @spec start(_Type, _StartArgs) -> ServerRet
 %% @doc application start callback for ginrummy.
 start(_Type, _StartArgs) ->
+    id_server:start_link(),
     ginrummy_deps:ensure(),
     ginrummy_sup:start_link().
 
 %% @spec stop(_State) -> ServerRet
 %% @doc application stop callback for ginrummy.
 stop(_State) ->
+    id_server:stop(),
     ok.
